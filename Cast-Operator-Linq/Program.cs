36f4bb50cd79c15233650bcd16588dde2090b203
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-11-09 12:10:55 +0330

    cast - oftype example

--- Cast-Operator-Linq/Program.cs
+++ Cast-Operator-Linq/Program.cs
@@ -2,18 +2,26 @@
 
 using System.Collections;
 
-List<int> numbers = new(Enumerable.Range(1, 11));  // we can use extension methos here because list coolection inherrited the IEnumerable interface
+List<int> numbers = new(Enumerable.Range(1, 11));  // we can use extension methos here because list coolection inherrited the IEnumerable<T> interface
 var result = numbers.Where(x => x > 7);  
 foreach (var item in result)
 {
     Console.WriteLine(item);
 }
 
-ArrayList array = new() { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, "Hello"};  // we don't have access to extension methos in linq here
-// becaus this collection doesn't inherrit the Enumerable class
+ArrayList array = new() { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };  // we don't have access to extension methos in linq here
+// becaus this collection doesn't inherrit the Enumerable<T> class
 
 //var result = array.Where(x => x > 7);      => error
 //foreach (var item in result)
 //{
 //    Console.WriteLine(item);
 //}
+
+var newCollection = array.Cast<int>();  // use cast to access the extension methods
+//var newCollection = array.OfType<int>();  // also we can use OfType an don't get the error because the string in array
+var result2 = newCollection.Where(x => x > 7);
+foreach (var item in result2)
+{
+    Console.WriteLine(item);
+}
